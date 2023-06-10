9966adfbb8da80d025d8b23d9de76057ae276f82
Franco Morales Castro <franco.morales@outlook.com>
2023-06-09 21:09:28 -0400

    feat: ✨ ejercicio numeros a palabras

--- Clase16/conversion.py
+++ Clase16/conversion.py
@@ -1,21 +1,69 @@
 # Funcion para convertir unidad a palabra
 def unidad_a_palabra(valor: str) -> str:
-    if valor == '1': return 'uno'
-    elif valor == '2': return 'dos'
-    elif valor == '3': return 'tres'
-    elif valor == '4': return 'cuatro'
-    elif valor == '5': return 'cinco'
-    elif valor == '6': return 'seis'
-    elif valor == '7': return 'siete'
-    elif valor == '8': return 'ocho'
-    elif valor == '9': return 'nueve'
+    unidad = valor[len(valor) - 1]
+    if unidad == '1': return 'uno'
+    elif unidad == '2': return 'dos'
+    elif unidad == '3': return 'tres'
+    elif unidad == '4': return 'cuatro'
+    elif unidad == '5': return 'cinco'
+    elif unidad == '6': return 'seis'
+    elif unidad == '7': return 'siete'
+    elif unidad == '8': return 'ocho'
+    elif unidad == '9': return 'nueve'
+    return ''
 
 # Funcion para convertir decena a palabra
 def decena_a_palabra(valor: str) -> str:
-    if valor == '10': return 'diez'
-    if valor == '11': return 'once'
-    if valor == '12': return 'doce'
-    if valor == '13': return 'trece'
-    if valor == '14': return 'catorce'
-    if valor == '15': return 'quince'
-    if valor[0] == '1' and int(valor[1]) > 5: return 'dieci'
+    unidad = valor[len(valor) - 1]
+    decena = valor[len(valor) - 2]
+    if decena == '1' and unidad == '0': return 'diez'
+    if decena == '1' and unidad == '1': return 'once'
+    if decena == '1' and unidad == '2': return 'doce'
+    if decena == '1' and unidad == '3': return 'trece'
+    if decena == '1' and unidad == '4': return 'catorce'
+    if decena == '1' and unidad == '5': return 'quince'
+    if decena == '1' and unidad == '6': return 'dieciseis'
+    if decena == '1' and unidad == '7': return 'diecisiete'
+    if decena == '1' and unidad == '8': return 'dieciocho'
+    if decena == '1' and unidad == '9': return 'diecinueve'
+    if decena == '2' and unidad == '0': return 'veinte'
+    if decena == '2' and int(unidad) >= 0: return 'veinti'
+    if decena == '3': return 'treinta'
+    if decena == '4': return 'cuarenta'
+    if decena == '5': return 'cincuenta'
+    if decena == '6': return 'sesenta'
+    if decena == '7': return 'setenta'
+    if decena == '8': return 'ochenta'
+    if decena == '9': return 'noventa'
+    return ''
+
+# Funcion para convertir centena a palabra
+def centena_a_palabra(valor: str) -> str:
+    unidad = valor[len(valor) - 1]
+    decena = valor[len(valor) - 2]
+    centena = valor[len(valor) - 3]
+    if centena == '1' and (len(decena) > 0 or len(unidad) > 0): return 'ciento'
+    elif centena == '1' and decena == '0' and unidad == '0': return 'cien'
+    if centena == '2': return 'doscientos'
+    if centena == '3': return 'trescientos'
+    if centena == '4': return 'cuatrocientos'
+    if centena == '5': return 'quinientos'
+    if centena == '6': return 'seiscientos'
+    if centena == '7': return 'setecientos'
+    if centena == '8': return 'ochocientos'
+    if centena == '9': return 'novecientos'
+    return ''
+
+# Funcion para convertir unidad de mil a palabras
+def unidad_de_mil_a_palabra(valor: str) -> str:
+    unidad_de_mil = valor[0]
+    if unidad_de_mil == '1': return 'mil'
+    if unidad_de_mil == '2': return 'dos mil'
+    if unidad_de_mil == '3': return 'tres mil'
+    if unidad_de_mil == '4': return 'cuatro mil'
+    if unidad_de_mil == '5': return 'cinco mil'
+    if unidad_de_mil == '6': return 'seis mil'
+    if unidad_de_mil == '7': return 'siete mil'
+    if unidad_de_mil == '8': return 'ocho mil'
+    if unidad_de_mil == '9': return 'nueve mil'
+    return ''
--- Clase16/main.py
+++ Clase16/main.py
@@ -1,5 +1,7 @@
 from conversion import unidad_a_palabra
 from conversion import decena_a_palabra
+from conversion import centena_a_palabra
+from conversion import unidad_de_mil_a_palabra
 
 def esUnValorValido(valor: int) -> bool:
     if valor <= 0 or valor > 9999:
@@ -11,12 +13,16 @@ while not esUnValorValido(int(valor)):
     print('Ingrese valores entre 1 y 9999')
     valor = input('Ingrese nuevamente un valor: ')
 
-cantidad_letras = len(valor)
-print(valor[cantidad_letras-2:cantidad_letras])
 palabra = ''
-if cantidad_letras >= 2:
-    palabra = decena_a_palabra(valor[cantidad_letras-2:cantidad_letras])
-if valor[cantidad_letras - 1] != '0':
-    palabra = palabra + unidad_a_palabra(valor[cantidad_letras - 1])
+if len(valor) >= 4:
+    palabra = palabra + unidad_de_mil_a_palabra(valor)
+if len(valor) >= 3:
+    palabra = palabra + ' ' + centena_a_palabra(valor)
+if len(valor) >= 2:
+    palabra = palabra + ' ' + decena_a_palabra(valor)
+if len(valor) >= 1 and valor[len(valor) - 1] != '0': 
+    if int(valor) > 29:
+        palabra = palabra + ' y'
+    palabra = palabra + ' ' + unidad_a_palabra(valor)
 
 print(palabra)
