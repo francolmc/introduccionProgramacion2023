0ad4d8c1d36da026e092c2fdae42caa5b0cfa577
francolmc <franco.morales@outlook.com>
2023-06-19 21:42:00 -0400

    feat: ✨ como funcionan los arreglos

---
+++ Clase19/main.py
@@ -0,0 +1,21 @@
+# Definir un arreglo
+mi_arreglo = [1, 2, 3]
+# Definir arreglo con distintos tipos
+mi_arreglo2 = [1, 2, 3, 'Pedro']
+
+# Acceder a un elemento del arreglo
+print('Obtener un elemento del arreglo: ', mi_arreglo[1])
+
+# Modificar o asignar un elemento a una posicion del arreglo
+mi_arreglo[2] = 8
+
+# Print arreglos
+print(mi_arreglo)
+print(mi_arreglo2)
+
+# Recorrer arreglos
+for elemento in mi_arreglo:
+    print(elemento)
+
+for elemento in mi_arreglo2:
+    print(elemento)
